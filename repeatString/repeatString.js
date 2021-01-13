9b8bf5548848d0776ad8f4cbcff5e97de11de3a3
JimBaba <aalberts.daniel@gmail.com>
2021-01-13 23:18:51 +0100

    solved helloworld.ja and repeatstring.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str = "", number) {
+    let fullStr = "";
+    if(number < 0){
+        return "ERROR"
+    }
+    for(let i = 1; i <= number; i++){
+        fullStr += str;
+    }
+    return fullStr;
 }
 
 module.exports = repeatString
